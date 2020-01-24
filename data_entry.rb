ffc76f83426f5a9096dc72e6200c73b4c49f92f9
Beth Ashton <Mbeth.ashton@gmail.com>
2020-01-24 17:25:10 -0500

    Add start to data entry step

---
+++ data_entry.rb
@@ -0,0 +1,8 @@
+puts "Welcome to the Data Entry program"
+puts "What is the name of this form notification?"
+notification_name = gets.chomp
+puts "Gravity forms has two main bits of info we need to make this work nicely:"
+puts " - Field Label"
+puts " - Field ID"
+puts "For each item you want in your notification email, you'll need to supply these two things."
+
